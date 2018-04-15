b20573750a1ac2866503b3aeb525742ee100318f
Egor Tumakov <el.tumakov@gmail.com>
2018-04-15 17:17:57 +0300

    Добавляет валидацию формы

--- js/pictures.js
+++ js/pictures.js
@@ -255,6 +255,10 @@ var pictureEditor = document.querySelector('.img-upload__overlay');
 var pictureUploadInput = document.querySelector('#upload-file');
 var resizeValue = document.querySelector('.resize__control--value');
 
+var inputHashTag = document.querySelector('.text__hashtags');
+var inputComment = document.querySelector('.text__description');
+var formSubmit = document.querySelector('.img-upload__submit');
+
 /**
  * Обработчик события при загрузке фотографии.
  * Открывает попап с редактором фотографии.
@@ -279,7 +283,7 @@ var pictureEditorCancel = document.querySelector('.img-upload__cancel');
  * @param {Object} evt - event.
  */
 var pictureEditorKeydownHandler = function (evt) {
-  if (evt.keyCode === ESC_KEYCODE) {
+  if (evt.keyCode === ESC_KEYCODE && !(inputHashTag === document.activeElement) && !(inputComment === document.activeElement)) {
     pictureEditorCancelClickHandler();
   }
 };
@@ -426,3 +430,73 @@ for (i = 0; i < effects.length; i++) {
  */
 
 scalePin.addEventListener('mouseup', getEffectLevel);
+
+
+/**
+ * Валидация формы загрузки изображения.
+ */
+
+/**
+ * Кастомная валидация поля с хэш-тегом.
+ */
+var inputHashTagCustomValidity = function () {
+  var hashTagValue = inputHashTag.value;
+  var separator = /\s+/;
+  var hashTags = hashTagValue.split(separator);
+  var errorMessages = [];
+  var error = false;
+
+  inputHashTag.setCustomValidity('');
+
+  for (i = 0; i < hashTags.length; i++) {
+    if (hashTags[i] === '') {
+      break;
+    }
+
+    if (!hashTags[i].match(/#/)) {
+      errorMessages.push('Хэш-тег должен начинаться с символа - #.');
+      error = true;
+    }
+
+    if (hashTags[i] === '#') {
+      errorMessages.push('Хэш-тег не может состоять только из одной решетки.');
+      error = true;
+    }
+
+    if (hashTags[i].match(/#[\wа-яё]+#/)) {
+      errorMessages.push('Хэш-теги должны разделяться пробелами.');
+      error = true;
+    }
+
+    if (hashTags[i] === hashTags[i - 1]) {
+      errorMessages.push('Один и тот же хэш-тег не может быть использован дважды.');
+      error = true;
+    }
+
+    if (hashTags.length > 5) {
+      errorMessages.push('Нельзя указывать больше пяти хэш-тегов.');
+      error = true;
+    }
+
+    if (hashTags[i].length > 20) {
+      errorMessages.push('Длина одного хэш-тега не должна превышать 20 символов (включая #).');
+      error = true;
+    }
+
+    if (error === true) {
+      inputHashTag.setCustomValidity(errorMessages.join('. \n'));
+      break;
+    }
+  }
+};
+
+/**
+ * Обработчик клика на кнопку "Опубликовать" в форме загрузки фотографии.
+ * Производит валидацию хэш-тега.
+ * Если валидация пройдена отправляет данные на сервер.
+ */
+var formSubmitClickHandler = function () {
+  inputHashTagCustomValidity();
+};
+
+formSubmit.addEventListener('click', formSubmitClickHandler);
